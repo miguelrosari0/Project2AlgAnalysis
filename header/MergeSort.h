34aa9838d37fb9a4365dc9981cd4e5ab49e57a30
miguelrosari0 <miguelrosario1469@gmail.com>
2026-03-26 09:33:33 -0400

    Added nessesary .h files

---
+++ header/MergeSort.h
@@ -0,0 +1,11 @@
+//
+// Created by Miguel Rosario on 3/17/26.
+//
+
+#ifndef MERGESORT_H
+#define MERGESORT_H
+#pragma once
+#include "Property.h"
+#include <vector>
+void mergeSort(std::vector<Property>& arr, SortAttribute sortBy);
+#endif //MERGESORT_H
